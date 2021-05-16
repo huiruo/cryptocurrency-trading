ea0c4ceababc6f0700eca5d4568d4db411d60ed7
huiruo <2196411859@qq.com>
2021-05-16 08:49:38 +0800

    feat:server

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,1 +1,28 @@
-module.exports = {}
+module.exports = {
+      // 公共路径
+      publicPath: "./",
+      chainWebpack: () => {},
+      // 配置 webpack-dev-server 行为。
+      devServer: {
+          open: true, //编译后默认打开浏览器
+          host: 'localhost',
+          port: 1688,
+          https: false,  //是否https
+          //显示警告和错误
+          overlay: {
+              warnings: false,
+              errors: true
+          },
+          /*proxy: {
+              '/api': {
+                  target: 'http://test.api',
+                  changeOrigin: true, 
+                  ws: false, 
+                  secure: false,
+                  pathRewrite: {
+                      '^/api': ''
+                  }
+              }
+          }*/
+      }
+}
