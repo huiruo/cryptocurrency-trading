7c08081a9fa3aa688f39c67997ed6d04ded2c167
huiruo <2196411859@qq.com>
2021-05-23 23:42:35 +0800

    feat:ui

--- web/src/components/Input.vue
+++ web/src/components/Input.vue
@@ -1,19 +1,31 @@
 <template>
-    <input :type="type" :placeholder="placeholder"/>
+  <span class="input-affix-wrapper">
+    <span class="input-suffix">{{ prefix }}</span>
+    <input class="rx-input" :type="type" :placeholder="placeholder" />
+    <span>{{ suffix }}</span>
+  </span>
 </template>
 
 <script>
 export default {
   name: "rxInput",
   components: {},
-    props: {
+  props: {
     type: {
       type: String,
-      default: "text"
+      default: "text",
     },
     placeholder: {
       type: String,
-      default: ""
+      default: "",
+    },
+    prefix: {
+      type: String,
+      default: "",
+    },
+    suffix: {
+      type: String,
+      default: "",
     },
     // onLoading: {
     //   type: Boolean,
@@ -28,9 +40,64 @@ export default {
     return {};
   },
   computed: {},
-  methods:{},
-  mounted() {}
+  methods: {},
+  mounted() {},
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.input-affix-wrapper {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 14px;
+  font-variant: tabular-nums;
+  line-height: 1.5;
+  list-style: none;
+  /* font-feature-settings: "tnum"; */
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  text-align: start;
+  .input-suffix {
+    position: absolute;
+    top: 50%;
+    z-index: 2;
+    display: flex;
+    align-items: center;
+    color: var(--main-color);
+    line-height: 0;
+    transform: translateY(-50%);
+    left: 12px;
+  }
+  .rx-input {
+    padding-left: 60px;
+    font-weight: 500;
+  }
+}
+.rx-input {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-variant: tabular-nums;
+  list-style: none;
+  font-feature-settings: "tnum";
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  min-width: 0;
+  padding: 4px 11px;
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 14px;
+  line-height: 1.5715;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+  transition: all 0.3s;
+}
+.rx-input:focus {
+  border-color: #41b37d;
+}
+</style>
--- web/src/views/index/index.vue
+++ web/src/views/index/index.vue
@@ -1,36 +1,95 @@
 <template>
-<div>
-  <Header />
   <div>
-    <rx-input placeholder="请输入购买数量"/>
-    <rx-input placeholder="请输入购买价格"/>
-    <rx-button @rxbClick="buyAction" title="购买"/>
-  </div>
+    <Header />
+    <div class="trade-action-container">
+      <div class="trade-left">left</div>
+      <div class="trade-middle">
+        <div class="action-part">
+          <div class="wrap-item">
+            <rx-input prefix="名称" placeholder="请输入购买名称" />
+          </div>
+          <div class="wrap-item">
+            <rx-input prefix="价格" placeholder="请输入购买价格" />
+          </div>
+          <div class="wrap-item">
+            <rx-input
+              style="margin-bottom: 8px"
+              prefix="数量"
+              placeholder="请输入购买数量"
+            />
+          </div>
+          <rx-button @rxbClick="buyAction" title="买入" />
+        </div>
+        <div class="action-part">
+          <div class="wrap-item">
+            <rx-input prefix="名称" placeholder="请输入卖出名称" />
+          </div>
+          <div class="wrap-item">
+            <rx-input prefix="价格" placeholder="请输入卖出价格" />
+          </div>
+          <div class="wrap-item">
+            <rx-input
+              style="margin-bottom: 8px"
+              prefix="数量"
+              placeholder="请输入卖出数量"
+            />
+          </div>
+          <rx-button @rxbClick="buyAction" title="卖出" />
+        </div>
+      </div>
+      <div class="trade-right">right</div>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header"
-import rxInput from "@/components/Input"
-import rxButton from "@/components/Botton"
+import Header from "@/components/Header";
+import rxInput from "@/components/Input";
+import rxButton from "@/components/Botton";
 export default {
   name: "index",
   components: {
     Header,
     rxInput,
-    rxButton
+    rxButton,
   },
   data() {
     return {};
   },
   computed: {},
-  methods:{
-    buyAction(){
-      console.log("buy11")
-    }
+  methods: {
+    buyAction() {
+      console.log("buy11");
+    },
   },
-  mounted() {}
+  mounted() {},
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.trade-action-container {
+  width: 1336px;
+  margin: 5px auto;
+  display: flex;
+  .trade-left {
+    width: 290px;
+    background: #009688;
+  }
+  .trade-middle {
+    display: flex;
+    width: 746px;
+    background: rgb(24 36 44);
+    .action-part {
+      width: 50%;
+      padding: 16px 16px 0;
+    }
+    .wrap-item {
+      margin-bottom: 8px;
+    }
+  }
+  .trade-right {
+    width: 290px;
+    background: #03a9f4;
+  }
+}
+</style>
