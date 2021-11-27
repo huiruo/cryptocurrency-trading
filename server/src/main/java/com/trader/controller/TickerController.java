91c10fe795aea1970da4f21717447c1451555ab4
zhouzhu85 <734837014@qq.com>
2021-11-27 22:54:27 +0800

    feat(server): ticker

--- server/src/main/java/com/trader/controller/TickerController.java
+++ server/src/main/java/com/trader/controller/TickerController.java
@@ -2,27 +2,33 @@ package com.trader.controller;
 
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.trader.emun.OkexApiEnum;
+import com.trader.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class TestController {
-    @GetMapping("ttttt")
-    public Map tess(){
-        Map map=new HashMap();
+@RequestMapping("api/spot/v3/instrument")
+public class TickerController {
 
-//        String ff = HttpUtil.get("https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker");
+    @Value("${OKEX_URL}")
+    private String OKEX_URL;
 
-        String result2 = HttpRequest.get("https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker")
+    @GetMapping("ticker")
+    public Result ticker(){
+        String result = HttpRequest.get(OKEX_URL+ OkexApiEnum.TICKER.getValue())
                 .setHttpProxy("127.0.0.1", 7890)
 //                .body(json)
                 .execute()
                 .body();
-
-        return map;
+        Map resultMap = JSON.parseObject(result, Map.class);
+        return new Result(resultMap);
     }
 }
