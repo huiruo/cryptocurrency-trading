bf156a3417c6b00e5e2f93f725348a783d4be8fc
huiruo <2196411859@qq.com>
2021-12-01 22:01:10 +0800

    feat(node): got请求代理

--- server_node/src/trader/user/user.controller.ts
+++ server_node/src/trader/user/user.controller.ts
@@ -1,13 +1,38 @@
-import { Controller,Body,Post } from '@nestjs/common';
+import { Controller,Body,Post,Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import got from 'got';
+import {HttpsProxyAgent} from 'hpagent';
 
-@Controller('user')
+@Controller('trader/ticker')
 export class UserController {
-    //测试使用post:http://localhost:1788/user/find
+    //http://localhost:1788/trader/ticker/ticker/find
     constructor(private readonly usersService:UserService){}
     @Post('find')
     findOne(@Body() body:any){
-        console.log("find----->")
         return this.usersService.findOne(body.username);
+    }
+
+    @Get('24hr')
+    ticker(){
+       //test:
+       //http://localhost:1788/trader/ticker/24hr?symbol=BTC-USDT&platform=okex
+       const ticker24URL = 'https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT'
+       const data= this.gotData(ticker24URL)
+       return data
+    }
+
+    async gotData(ticker24URL:string){
+        const data = await got.get(ticker24URL,{
+            agent: {
+                https: new HttpsProxyAgent({
+                    keepAlive: true,
+                    keepAliveMsecs: 10000,
+                    maxSockets: 256,
+                    maxFreeSockets: 256,
+                    proxy: 'http://127.0.0.1:7890'
+                })
+            }
+        }).json();
+        return data
     }
 }
