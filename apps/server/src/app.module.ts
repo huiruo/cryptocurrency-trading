fdeb2178e2fc5bf5a8de6bb05f0b08977a7018a0
ruo <ruomechen@gmail.com>
2023-07-22 21:59:01 +0800

    feat: link Binance api

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -10,9 +11,13 @@ import { MarketCenterModule } from './crypto/market-center/market.center.module'
 import { SpotModule } from './crypto/spot/spot.module'
 import { FutureModule } from './crypto/future/future.module'
 import { StrategyOrderModule } from './crypto/strategy-order/strategy.order.module'
+import { getDirFilenames } from './common/utils/getDirFilenames'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [...getDirFilenames({ environment: process.env.NODE_ENV })],
+    }),
     TypeOrmModule.forRoot(dbConfig),
     CodeEngineModule,
     UserModule,
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { BinanceService } from './crypto/common/binance-service'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  const configService = app.get(ConfigService)
+  const apiKey = configService.get<string>('binanceApiKey')
+  const secretKey = configService.get<string>('binanceSecretKey')
+  if (apiKey && secretKey) {
+    console.log('bootstrap init base', { apiKey, secretKey })
+    BinanceService.getInstance().connect(apiKey, secretKey)
+  } else {
+    console.log('=== Api key do not exist ===')
+  }
+
   await app.listen(3888)
 }
 
