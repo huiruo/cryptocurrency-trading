765a5a72b3ca80212d832ab76dd32b6a4211444f
ruo <ruomechen@gmail.com>
2023-07-22 16:28:03 +0800

    feat: jwt expiresIn

--- apps/server/src/common/constant.ts
+++ apps/server/src/common/constant.ts
@@ -4,6 +4,6 @@ export const fail = 0
 export const jwtConstants = {
   // expiresIn: '60s', // test
   // expiresIn: '120s', // test
-  // expiresIn: '3600s', // nornal
-  expiresIn: '300s',
+  expiresIn: '3600s', // nornal
+  // expiresIn: '300s',
 }
