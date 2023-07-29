dd0025de470c18ad42caf36e4dd0f1054af2f806
ruo <ruomechen@gmail.com>
2023-07-29 11:22:22 +0800

    improve: Interceptor

--- apps/server/src/user/auth.guard.ts
+++ apps/server/src/user/auth.guard.ts
@@ -81,7 +81,7 @@ export class AuthGuard implements CanActivate {
 
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
-      request['user'] = payload
+      // request['user'] = payload
     } catch {
       console.log('auth.guard reject-->')
 
